869e2db040ec9978af4321c3fc7b611566059cd3
oussama smaoui <osmaoui1996@gmail.com>
2024-07-01 12:05:30 +0100

    add: validate.py

--- evaluation/validate.py
+++ evaluation/validate.py
@@ -6,23 +6,25 @@ Prediction file should be a 4-col CSV file.
 
 import argparse
 import json
+import pickle
 
 import pandas as pd
 
-INDEX = ["Dataset", "Mixture_1", "Mixture_2"]
-COLS = {
-    "Dataset": str,
-    "Mixture_1": int,
-    "Mixture_2": int,
-    "Predicted_Experimental_Values": float,
-}
 
+COLS = {
+    "key": str,
+    "coord_x": float,
+    "coord_y": float,
+    "coord_z": float,
+    "class": str,
+    "score": float
+}
+LANDMARKS_TYPE = ["Mesial", "Distal", "Cusp", "InnerPoint", "OuterPoint", "FacialPoint"]
 
 def get_args():
     """Set up command-line interface and get arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--predictions_file", type=str, required=True)
-    parser.add_argument("-g", "--goldstandard_file", type=str, required=True)
     parser.add_argument("-e", "--entity_type", type=str, required=True)
     parser.add_argument("-o", "--output", type=str)
     return parser.parse_args()
@@ -35,28 +37,6 @@ def check_dups(pred):
         return (
             f"Found {duplicates.sum()} duplicate mixture(s): "
             f"{pred[duplicates].index.to_list()}"
-        )
-    return ""
-
-
-def check_missing_mixtures(gold, pred):
-    """Check for missing mixtures."""
-    missing_ids = gold.index.difference(pred.index)
-    if len(missing_ids) > 0:
-        return (
-            f"Found {missing_ids.shape[0]} missing mixture(s): "
-            f"{missing_ids.to_list()}"
-        )
-    return ""
-
-
-def check_unknown_mixtures(gold, pred):
-    """Check for unknown mixtures."""
-    unknown_ids = pred.index.difference(gold.index)
-    if len(unknown_ids) > 0:
-        return (
-            f"Found {unknown_ids.shape[0]} unknown mixture(s): "
-            f"{unknown_ids.to_list()}"
         )
     return ""
 
@@ -75,24 +55,28 @@ def check_prob_values(pred, col):
         return f"'{col}' column should be between [0, 1] inclusive."
     return ""
 
+def check_class_values(pred, col, valid_classes):
+    """Check if class column contains only valid landmarks."""
+    invalid_classes = pred[~pred[col].isin(valid_classes)]
+    if not invalid_classes.empty:
+        return (
+            f"'{col}' column contains invalid values: "
+            f"{invalid_classes[col].unique().tolist()}"
+        )
+    return ""
 
-def validate(gold_file, pred_file):
+
+def validate(pred_file):
     """Validate predictions file against goldstandard."""
     errors = []
-
-    gold = pd.read_csv(gold_file)
-
-    # Replace spaces in column headers in case they're found.
-    gold.columns = [colname.replace(" ", "_") for colname in gold.columns]
-    gold.set_index(INDEX, inplace=True)
 
     try:
         pred = pd.read_csv(
             pred_file,
             usecols=COLS,
             dtype=COLS,
-            float_precision="round_trip",
-            index_col=INDEX,
+            float_precision="high",
+            sep=','
         )
     except ValueError:
         errors.append(
@@ -101,10 +85,11 @@ def validate(gold_file, pred_file):
         )
     else:
         errors.append(check_dups(pred))
-        errors.append(check_missing_mixtures(gold, pred))
-        errors.append(check_unknown_mixtures(gold, pred))
-        errors.append(check_nan_values(pred, "Predicted_Experimental_Values"))
-        errors.append(check_prob_values(pred, "Predicted_Experimental_Values"))
+        errors.append(check_class_values(pred, "class", LANDMARKS_TYPE))
+        errors.append(check_nan_values(pred, "coord_x"))
+        errors.append(check_nan_values(pred, "coord_y"))
+        errors.append(check_nan_values(pred, "coord_z"))
+        errors.append(check_prob_values(pred, "score"))
     return errors
 
 
@@ -117,7 +102,7 @@ def main():
         invalid_reasons = [f"Submission must be a File, not {entity_type}."]
     else:
         invalid_reasons = validate(
-            gold_file=args.goldstandard_file, pred_file=args.predictions_file
+            pred_file=args.predictions_file
         )
 
     invalid_reasons = "\n".join(filter(None, invalid_reasons))
